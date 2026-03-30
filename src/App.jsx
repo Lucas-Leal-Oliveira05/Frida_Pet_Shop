46c4a5f84833251f9b01ad8aff63ffc7bc894504
Lucas-Leal-Oliveira05 <lucas.l.oliveira0509@gmail.com>
2026-03-30 10:49:39 -0300

    Home semi-pronta
    estruturação da UI da página Home concluída. Falta criar e inserir as artes da página. (por enquanto página sem funcionalidades, apenas interface)

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,10 @@ function App(){
   return(
     <div>
       <Header/>
-      <section className="w-full  flex justify-between bg-[#F1E3C6]"> {/* Sec 1 */}
+      {/* Sec 1 */}
+      <section className="w-full  flex justify-between bg-[#F1E3C6]">
         <div className="justify-center-safe flex">
-          {/*escrita + botão - div 1 */}
+          {/*escrita + botão */}
           <div className="w-96 flex flex-col text-[#7A5A3F] justify-center gap-5 pl-20"> 
             <h1 className="text-3xl">Cuidado e carinho para o seu pet</h1>
             <p>Banho, tosa e produtos</p>
@@ -20,27 +21,49 @@ function App(){
         </div>
         <div className="justify-center-safe flex pr-20"> 
           <div>
-            {/*Imagem - div 1 */}
+            {/*Imagem */}
             <img className="pt-5 pb-5"  src="https://placehold.co/850x750" alt="" />
           </div>          
         </div>
       </section>
 
-      <section className="w-full flex flex-col items-center bg-[#F6EBDD]"> {/* Sec 2 */}
+      {/* Sec 2 */}
+      <section className="w-full flex flex-col items-center bg-[#F6EBDD]">
         <h1 className="pt-10 text-3xl text-[#7A5A3F] ">Nossos serviços</h1>
         <div className="w-full flex flex-row justify-between items-center pb-20 pt-10 pl-20 pr-20">
-          <div className="w-96 h-96 bg-[#F6EDDF] shadow-xl rounded-4xl flex justify-center items-center flex-col">
-            <div className="h-28 w-28 rounded-full bg-[#5FA79B] flex"></div>
-            <h1>Banho</h1>
-            <h2>Higiene completa e cheirosa</h2>
+          {/* Card Banho */}
+          <div className="w-96 h-96 bg-[#F6EDDF] shadow-xl rounded-4xl flex items-center flex-col gap-10">
+            <div className="h-36 w-36 rounded-full bg-[#5FA79B] flex mt-10"></div>
+            <div className="items-center flex flex-col text-[#7A5A3F] gap-5">
+              <h1 className="text-4xl" >Banho</h1>
+              <h2 className="text-lg">Higiene completa e cheirosa</h2>
+            </div>
           </div>
-
-          <div className="w-96 h-96 bg-[#F6EDDF] shadow-xl rounded-4xl"></div>
-
-          <div className="w-96 h-96 bg-[#F6EDDF] shadow-xl rounded-4xl"></div>
-
+          {/* Card tosa*/}
+          <div className="w-96 h-96 bg-[#F6EDDF] shadow-xl rounded-4xl flex items-center flex-col gap-10">
+            <div className="h-36 w-36 rounded-full bg-[#E38870] flex mt-10"></div>
+            <div className="items-center flex flex-col text-[#7A5A3F] gap-5">
+              <h1 className="text-4xl" >Tosa</h1>
+              <h2 className="text-lg">Estilo e conforto para seu pet</h2>
+            </div>
+          </div>
+          {/* Card loja*/}
+          <div className="w-96 h-96 bg-[#F6EDDF] shadow-xl rounded-4xl flex items-center flex-col gap-10">
+            <div className="h-36 w-36 rounded-full bg-[#F6CF8F] flex mt-10"></div>
+            <div className="items-center flex flex-col text-[#7A5A3F] gap-5">
+              <h1 className="text-4xl" >Loja</h1>
+              <h2 className="text-lg">Rações e brinquedos premium</h2>
+            </div>
+          </div>
         </div>
       </section>
+
+      {/* Sec 3 */}
+      <section className="bg-[#F1E3C6] flex flex-col justify-center items-center gap-5 pb-10"> 
+        <h1 className="text-[#7A5A3F] text-3xl pt-10"> Seu pet merece o melhor</h1>
+        <button className="rounded-md bg-[#E67C73] px-6 py-3 text-sm font-medium text-white w-52">Falar no whatsapp</button>
+      </section>
+
       <Footerx/>
     </div>
   );
