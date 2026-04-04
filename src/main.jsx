8d6de06590d13076e50ff6df5a304519c6abea48
Lucas-Leal-Oliveira05 <lucas.l.oliveira0509@gmail.com>
2026-04-04 15:57:13 -0300

    Tela EditarPet
    Tela de editar informações do pet implementada. sem back-end

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import CadastroPet from './pages/CadastroPet.jsx'
+import EditarPet from './pages/EditarPet.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <CadastroPet/>
+    <EditarPet/>
   </StrictMode>
 )
