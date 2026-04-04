94ef14d5fdb183c8ed2f90f01aee8446386fbac9
Gabriel Vitorio Goncalves <104917512+GabrielVitorioGoncalves@users.noreply.github.com>
2026-04-04 13:09:48 -0300

    feat: correção de bugs

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,10 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
-console.log("URL do Supabase:", import.meta.env.VITE_SUPABASE_URL);
-console.log("Chave do Supabase:", import.meta.env.VITE_SUPABASE_ANON_KEY);
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonkey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_DEFAULT_KEY;
-console.log("Chave do Supabase:", import.meta.env.VITE_SUPABASE_PUBLISHABLE_DEFAULT_KEY);
 
 export const supabase = createClient(supabaseUrl, supabaseAnonkey)
 
